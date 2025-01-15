6b359bb0492c3bb7e4763ab3c411a55f1f67a3b6
munisa <munisamusayevaa@gmail.com>
2025-01-15 16:01:16 +0500

    swagger ulandi va auth guard yaratildi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,43 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { appConfig } from './config/app.config';
 import { dbConfig } from './config/db.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './guard/authGuard';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [appConfig, dbConfig],
-      isGlobal: true
+      isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        type: "postgres",
-        port: configService.get<number>('dbConfig.port'),
+        type: 'postgres',
         host: configService.get<string>('dbConfig.host'),
+        port: configService.get<number>('dbConfig.port'),
         username: configService.get<string>('dbConfig.user'),
         password: configService.get<string>('dbConfig.password'),
         database: configService.get<string>('dbConfig.dbName'),
-        entites: [],
-        autoLoadEntities:true,
+        entities: [],
+        autoLoadEntities: true,
         synchronize: true,
       }),
-      inject: [ConfigService]
+      inject: [ConfigService],
     }),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    // {
+    //   provide: APP_GUARD,
+    // useClass: RolesGuard,
+    // },
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,31 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const appConfig = app.get(ConfigService)
+  const appConfig = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ashyo API')
+    .setDescription('API documentation for Ashyo App')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(
     appConfig.get<number>('appConfig.port'),
-    appConfig.get<string>('appConfig.host'), () => {
-      console.log(`Server running port on ${appConfig.get<number>('appConfig.port')}`)
-    });
+    appConfig.get<string>('appConfig.host'),
+    () => {
+      console.log(
+        `Server running on http://${appConfig.get<string>('appConfig.host')}:${appConfig.get<number>('appConfig.port')}`,
+      );
+    },
+  );
 }
 bootstrap();
