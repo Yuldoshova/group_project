c8af0ca6b9c1dc7eba5871b7d6429d0ab5488ac6
Yuldoshova <ozoda8171@gmail.com>
2025-01-16 17:01:29 +0500

    added relations for category

--- src/modules/categories/entities/category.entities.ts
+++ src/modules/categories/entities/category.entities.ts
@@ -1,7 +1,12 @@
+import { Brand } from 'modules/brand/entities/brand.entity';
+import { Product } from 'modules/product/entities/product.entity';
+import { Variation } from 'modules/variation/entities/variation.entity';
 import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
@@ -14,17 +19,23 @@ export class Category {
   @Column({ name: 'name', type: 'varchar', nullable: true })
   name: string;
 
-  @Column({ name: 'brend_id', type: 'integer', nullable: true })
-  brend_id: number;
+  @ManyToOne(() => Brand, (brand) => brand.categories)
+  brand: Brand;
 
-  @Column({ name: 'parent_id', type: 'integer', nullable: false })
-  parent_id: number;
+  @ManyToOne(() => Category, (category) => category.id)
+  parent: Category;
 
   @Column({ name: 'image', type: 'varchar', nullable: true })
   image: string;
 
   @Column({ name: 'icon', type: 'varchar', nullable: true })
   icon: string;
+
+  @OneToMany(() => Product, (product) => product.category)
+  products: Array<Product>
+
+  @OneToMany(() => Variation, (variation) => variation.category)
+  variations: Array<Variation>
 
   @CreateDateColumn({
     name: 'created_at',
