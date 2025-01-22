a365556fa4089cfdfe02ff9907dfaa4be4abdd43
Yuldoshova <ozoda8171@gmail.com>
2025-01-22 11:48:12 +0500

    auth done

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,14 +1,15 @@
-// import { Module } from '@nestjs/common';
-// import { AuthService } from './auth.service';
-// import { AuthController } from './auth.controller';
-// import { UserService } from '../user/user.service';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { User } from '../user/entities/user.entity';
-// // import { RedisService } from 'client/redis.service';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RedisService } from '@redis';
+import { User } from '../user/entities';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserService } from '../user';
+import { UploadService } from '../upload';
 
-// @Module({
-//   imports: [TypeOrmModule.forFeature([User])],
-//   controllers: [AuthController],
-//   providers: [ AuthService, UserService],
-// })
-// export class AuthModule {}
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [AuthController],
+  providers: [AuthService, UserService, RedisService, UploadService],
+})
+export class AuthModule { }
